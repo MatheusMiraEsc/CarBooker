a30c37bbead6800f09f62df94cff3b8dc126812d
Matheus Miranda <mme@cesar.school>
2024-05-13 19:03:10 -0300

    Adicionado funcionalidades de atualização e deletar usuarios

--- usuarios_teste.py
+++ usuarios_teste.py
@@ -19,9 +19,9 @@ def menu2():
         elif opcao == "2":
             visualizar_usuario(arquivo_json)
         elif opcao == "3":
-            print("Funcionalidade indisponível")
+            atualizar_usuario(arquivo_json)
         elif opcao == "4":
-            print("Funcionalidade indisponível")
+            deletar_usuario(arquivo_json)
         elif opcao == "5":
             break
         else:
@@ -30,7 +30,7 @@ def menu2():
 
 def cadastrar_usuario():
     chaves = ["Nome", "Sobrenome", "Data de nascimento",
-              "CPF", "CNH", "Gênero", "Telefone", "Email", "Senha"]
+              "CPF", "CNH", "Genero", "Telefone", "Email", "Senha"]
     usuarios = {}
     for chave in chaves:
         dados_usuario = input(f"Digite seu(sua) {chave}: ")
@@ -48,7 +48,7 @@ def add_usuario(usuario, arquivo):
         print("Ocorreu um erro:", e)
 
     numero_usuarios = len(dados)+1
-    nome_usuario = f"Usuário {numero_usuarios}"
+    nome_usuario = f"Usuario {numero_usuarios}"
     dados[nome_usuario] = usuario
 
     try:
@@ -67,7 +67,7 @@ def visualizar_usuario(arquivo):
             nome_usuario = f"Usuário {numero_usuarios}"
         nome = input(
             "Digite o nome do usuário que deseja visualizar: ")
-        senha = input("Digite a senha do usuário: ")
+        senha = input("Digite a sua senha: ")
         usuario_encontrado = False
         for dados, info in usuario.items():
             if info["Nome"] == nome and info["Senha"] == senha:
@@ -86,6 +86,81 @@ def visualizar_usuario(arquivo):
         print("Ocorreu um erro:", e)
 
 
+def atualizar_usuario(arquivo):
+    try:
+        with open(arquivo, "r+") as f:
+            usuario = json.load(f)
+            numero_usuarios = len(usuario)
+            nome_usuario = f"Usuário {numero_usuarios}"
+            nome = input(
+                "Digite o nome do usuário que deseja atualizar: ")
+            usuario_encontrado = False
+            for dados, info in usuario.items():
+                if info["Nome"] == nome:
+                    senha = input("Digite a sua senha: ")
+                    if info["Senha"] == senha:
+                        usuario_encontrado = True
+                        print("\n==============================\n")
+                        print(nome_usuario)
+                        for chave, valor in info.items():
+                            print(f"{chave}: {valor}")
+                        print("\n==============================\n")
+                        chave = input("Digite a chave que deseja atualizar: ")
+                        valor = input("Digite a informação a ser atualizada: ")
+                        info[chave] = valor
+                        f.seek(0)
+                        json.dump(usuario, f, indent=4)
+                        print("Usuário atualizado com sucesso!")
+                        break
+        if not usuario_encontrado:
+            print("Usuário não encontrado ou senha incorreta.")
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+    except Exception as e:
+        print("Ocorreu um erro:", e)
+
+
+def deletar_usuario(arquivo):
+    try:
+        with open(arquivo, "r+") as f:
+            usuarios = json.load(f)
+            numero_usuarios = len(usuarios)
+            nome_usuario = f"Usuario {numero_usuarios}"
+            nome = input(
+                "Digite o nome do usuário que deseja deletar: ")
+            usuario_encontrado = False
+            for dados, info in list(usuarios.items()):
+                if info["Nome"] == nome:
+                    senha = input("Digite a sua senha: ")
+                    if info["Senha"] == senha:
+                        usuario_encontrado = True
+                        print("\n==============================\n")
+                        print(nome_usuario)
+                        for chave, valor in info.items():
+                            print(f"{chave}: {valor}")
+                        print("\n==============================\n")
+                        conf = input(
+                            "Você realmente deseja deletar seu perfil? (S ou N) -> ")
+                        if conf.lower() == "s":
+                            del usuarios[dados]
+                            print("Usuário deletado com sucesso!")
+                        else:
+                            print("Deletação do usuário cancelada!")
+                            break
+                    f.seek(0)
+                    json.dump(usuarios, f, indent=4)
+                    break
+        if not usuario_encontrado:
+            print("Usuário não encontrado ou senha incorreta.")
+        else:
+            with open(arquivo, "w") as f:
+                json.dump(usuarios, f, indent=4)
+    except FileNotFoundError:
+        print("Arquivo não encontrado")
+    except Exception as e:
+        print("Ocorreu um erro:", e)
+
+
 def main():
 
     while True:
@@ -97,8 +172,6 @@ def main():
         print("3. Sair")
         print("============================")
         arquivo_json = "usuarios.json"
-        caminho_absoluto = os.path.abspath(arquivo_json)
-        print("Caminho absoluto do arquivo:", caminho_absoluto)
         opcao = input("Escolha uma opção: ")
         if opcao == "1":
             menu2()
